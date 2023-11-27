7ff7795d53c7ce4bd59218e57e3d50ac48e5fbdc
eangao <elmarcera@gmail.com>
2023-11-27 16:43:45 +0800

    Simple Array Methods

--- script.js
+++ script.js
@@ -74,3 +74,181 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+// Now before we start, let's quickly talk about why arrays
+// do actually have methods.
+// Well, remember that methods are simply functions
+// that we can call on objects.
+// So basically, they are functions attached to objects.
+// So if we have array methods, that means that
+// arrays themselves are also objects.
+// And so these array methods are simply functions
+// that are attached to all arrays
+// that we create in JavaScript.
+
+// Now, we will learn why all arrays have access
+// to this methods in a later section
+// when we talk about prototypal inheritance.
+// But for now, I just want you to understand that arrays
+// are objects, and that they get access to special
+// built in methods that we can essentially
+// see as tools for arrays.
+// And so as I mentioned, in this lecture, we're gonna start
+// by learning some very simple tools
+// that we can use on arrays.
+
+let arr = ['a', 'b', 'c', 'd', 'e'];
+
+//=================
+// SLICE METHOD
+
+// Alright, the first method we're gonna talk about
+// is the slice method.
+// And this one is very similar to the slice method
+// that's available on strings, remember?
+// So with the slice method, we can extract part of any array,
+// but without changing the original array.
+// So essentially, we can take a slice of an array.
+// And so that's why it's called slice.
+// So array.slice, and now just like in strings,
+// we have the begin parameter.
+
+console.log(arr.slice(2));
+// will return a new array
+// So as I said, this does not mutate the original arr, array.
+// Instead it returns a new array.
+// So a copy of the array, but only with the extracted parts.
+
+console.log(arr.slice(2, 4));
+// the end parameter here is not included in the output.
+
+// And so the length of the output array right here
+// will be the end parameter minus the beginning one.
+
+console.log(arr.slice(-2));
+console.log(arr.slice(-1));
+// So minus one will basically take the last two elements
+// of the array, so D and E.
+// And so with this, it's pre
+
+console.log(arr.slice(1, -2));
+
+// to simply create a shallow copy of any array.
+console.log(arr.slice());
+
+// something similar in one of the previous sections,
+// but using the spread operator, remember?
+// So back then we did this.
+// So creating a new array, and then expanding
+// the original array into that.
+// And so that gives us the exact same result.
+console.log([...arr]);
+
+// So the question is, should you use the spread operator
+// or the slice method, in order to create a shallow copy?
+// Well, that's actually entirely up to you.
+// So it's just a matter of personal preference.
+// The only time you really need to use the slice method here
+// is when you want to chain multiple methods together
+// so calling one after the other.
+// And we will do that, of course, later in this section.
+// All right.
+// So that is the slice method.
+
+//================
+// SPLICE
+
+// And a splice method works in almost the same way as slice.
+// But the fundamental difference is that
+// it does actually change the original array.
+// So it mutates that array.
+
+// console.log(arr.splice(2));
+
+// But now, watch what happened to our original array,
+// and we see that all that remains here in our original array
+// is the first two elements.
+// And so now the extracted elements are actually gone
+// from the original array.
+// So splice deleted them.
+// And so what we can see is that splice actually does mutate
+// the original array, so it takes part of the array
+// and returns it and then the original array itself
+// loses this part that was extracted.
+console.log(arr);
+
+// Now in practice, most of the time the value that
+// the splice method returns, doesn't even interest us.
+// All we are usually interested in is to just delete
+// one or more elements from an array using splice.
+// And one pretty common use case is to simply remove
+// the last element of an array.
+arr.splice(-1);
+console.log(arr);
+arr.splice(1, 2); //number of element to delete
+console.log(arr);
+
+// splice reference
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
+
+//===============
+// REVERSE
+arr = ['a', 'b', 'c', 'd', 'e'];
+const arr2 = ['j', 'i', 'h', 'g', 'f'];
+console.log(arr2.reverse());
+
+// But now what's important to note here is the fact
+// that the reverse method does actually mutate
+// the original array.
+console.log(arr2);
+
+//==================
+// CONCAT
+
+// And so this one is used to concatenate two arrays.
+
+const letters = arr.concat(arr2);
+console.log(letters);
+
+// And once again, we already did this before,
+// in another way, remember?
+console.log([...arr, ...arr2]);
+
+// This gives us the exact same result
+// and it also does not mutate any of the involved arrays.
+// So just like concat, which also doesn't mutate
+// the original array here, alright.
+
+// And by the way, here in this concat method,
+// so if you prefer to do this one is of course perfectly fine.
+// And once again, it is just a matter of personal preference,
+// whether you prefer to use the spread operator,
+// or the concat method.
+// All right.
+
+//================
+//JOIN
+
+// And finally, just the join method
+// that we already talked about before,
+// but just for the sake of completion here, right.
+
+console.log(letters.join(' - '));
+
+// Okay, and so as you already know, the result here
+// is a string with the separator that we specified.
+// Great.
+
+// So your array tool set is rapidly growing here.
+// And remember that you already know push on shift,
+// pop shift, index of n includes from the intersection.
+// Now if you ever lose track of all these different methods,
+// and how they work, you can always come back to these videos.
+
+// Or of course, check the documentation on MDN,
+// just like we did here.
+
+// So no developer in the world knows all of this by heart.
+// So sometimes I even have to look up how the splice
+// or the slice method works,
+// because it's just too many methods to keep track of.
+// And, yeah, it's very hard to know everything by heart.
