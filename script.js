8731ac9b1ae1cef64a972505beaf64c4b759b1d4
elmar <elmarcera@gmail.com>
2023-11-28 09:45:56 +0800

    forEach With Maps and Sets

--- script.js
+++ script.js
@@ -65,12 +65,6 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
-
 // const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
@@ -320,191 +314,272 @@ const currencies = new Map([
 // Looping Arrays: forEach
 ////////////////////////////////////////////////////////////
 
-// In this lecture we will loop over an array
-// using the forEach method.
-// Now we had already learned
-// how to loop over an array using the for of loop,
-// but the forEach method is really fundamentally different.
+// // In this lecture we will loop over an array
+// // using the forEach method.
+// // Now we had already learned
+// // how to loop over an array using the for of loop,
+// // but the forEach method is really fundamentally different.
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-// So these positive values here are basically deposits
-// and the negative values are withdrawals.
+// // So these positive values here are basically deposits
+// // and the negative values are withdrawals.
 
-for (const movement of movements) {
-  if (movement > 0) {
-    console.log(`You deposited ${movement}`);
-  } else {
-    console.log(`You withdraw ${Math.abs(movement)}`);
-  }
-}
+// for (const movement of movements) {
+//   if (movement > 0) {
+//     console.log(`You deposited ${movement}`);
+//   } else {
+//     console.log(`You withdraw ${Math.abs(movement)}`);
+//   }
+// }
 
-// but now let's finally learn how to use the forEach method
-// to achieve the exact same thing
-// but in my opinion in an easier way.
-// So to loop over the movements array
-// we use forEach,
-// and that's with a capital E, don't forget that.
-// And then the forEach method
-// actually requires a callback function here.
-// So forEach is technically a higher order function
-// as we learned in the last section,
-// which requires a callback function
-// in order to tell it what to do.
+// // but now let's finally learn how to use the forEach method
+// // to achieve the exact same thing
+// // but in my opinion in an easier way.
+// // So to loop over the movements array
+// // we use forEach,
+// // and that's with a capital E, don't forget that.
+// // And then the forEach method
+// // actually requires a callback function here.
+// // So forEach is technically a higher order function
+// // as we learned in the last section,
+// // which requires a callback function
+// // in order to tell it what to do.
 
-// So it's the forEach method here
-// that will call this callback function.
-// We are not calling it ourselves as always.
-// And that's of course important to keep in mind once again,
-// But when exactly will forEach actually
-// call this callback function.
-// Well what the forEach method does is to loop over the array,
-// and in each iteration it will execute
-// this callback function here.
-// Also as the forEach method calls this callback function
-// in each iteration it will pass in the current element
-// of the array as an argument,
+// // So it's the forEach method here
+// // that will call this callback function.
+// // We are not calling it ourselves as always.
+// // And that's of course important to keep in mind once again,
+// // But when exactly will forEach actually
+// // call this callback function.
+// // Well what the forEach method does is to loop over the array,
+// // and in each iteration it will execute
+// // this callback function here.
+// // Also as the forEach method calls this callback function
+// // in each iteration it will pass in the current element
+// // of the array as an argument,
 
-// and we can specify that here
-// and let's call that again movement.
-// So again, each time this callback here is called,
-// so in each iteration,
-// it will receive the current element of the array
-// as an argument.
+// // and we can specify that here
+// // and let's call that again movement.
+// // So again, each time this callback here is called,
+// // so in each iteration,
+// // it will receive the current element of the array
+// // as an argument.
 
-console.log('------FOREACH-------');
-movements.forEach(function (movement) {
-  if (movement > 0) {
-    console.log(`You deposited ${movement}`);
-  } else {
-    console.log(`You withdraw ${Math.abs(movement)}`);
-  }
+// console.log('------FOREACH-------');
+// movements.forEach(function (movement) {
+//   if (movement > 0) {
+//     console.log(`You deposited ${movement}`);
+//   } else {
+//     console.log(`You withdraw ${Math.abs(movement)}`);
+//   }
+// });
+
+// // 0: function(200)
+// // 1: function(450)
+// // 2: function(400)
+// // ...
+
+// // So basically this is exactly the concept that I explained
+// // In the last section
+// // when I said that we use a callback function
+// // to tell another higher order function
+// // exactly what it should do,
+// // and so in this case we tell forEach
+// // that in each iteration
+// // it should log one of these two strings here to the console
+// // So we give the forEach method instructions
+// // by giving it a callback function
+// // which contains these instructions, alright.
+// // And I really know and understand
+// // that this is quite a hard concept to wrap your head around.
+// // But if you just continue using this from now on
+// // then eventually it will start to make sense, believe me.
+
+// // Well I think that we can both agree
+// // that it is the forEach method, right.
+// // And maybe you don't agree with that
+// // and of course that's okay,
+// // as I keep mentioning
+// // it's always good to develop your own style of programming.
+
+// // So entries is in fact just another array method
+// // and it returns an array of arrays,
+// // remember, which in the first position
+// // contains the current index
+// // and then the value itself.
+// // And so this is how we access
+// // the counter variable in the for of loop.
+// for (const [i, movement] of movements.entries()) {
+//   if (movement > 0) {
+//     console.log(`Movement ${i + 1}: You deposited ${movement}`);
+//   } else {
+//     console.log(`Movement ${i + 1}: You withdraw ${Math.abs(movement)}`);
+//   }
+// }
+
+// // And here, fortunately, it's a lot easier
+// // to get access to the current index.
+// // So to understand how it works we need to remember once more
+// // that it is the forEach method
+// // who calls this callback function in each iteration.
+// // And as it calls this function
+// // it also passes in the current element of the array,
+// // but actually that's not all it passes in
+
+// // in fact forEach passes in the current element,
+// // the index and the entire array that we are looping.
+// // And so therefore we can specify them here
+// // in our parameter list.
+// // So let's say index and array,
+
+// // now of course we can just use one, like we have been doing,
+// // or we can just use two,
+// // or we can use all three together.
+// // And as always the names here do not matter at all,
+// // but what does matter is the order.
+// // So the first parameter always needs to be
+// // the current element,
+// // the second parameter always the current index
+// // and the third one always the entire array
+// // that we are looping over.
+// // Because that's the order in which the arguments,
+// // so the actual values, are passed into our callback function.
+
+// // And in fact in the real world we actually use
+// // shorter names.
+// console.log('------FOREACH with index-------');
+// movements.forEach(function (mov, i, arr) {
+//   if (mov > 0) {
+//     console.log(`Movement ${i + 1}: You deposited ${mov}`);
+//   } else {
+//     console.log(`Movement ${i + 1}: You withdraw ${Math.abs(mov)}`);
+//   }
+// });
+
+// // Now when should you use forEach
+// // and when should you use the for of loop.
+
+// // Well one fundamental difference
+// // between the two of them is that you cannot break out
+// // of a forEach loop.
+// // So the continue and break statements
+// // do not work in a forEach loop at all.
+// // So instead, forEach will always loop over the entire array
+// // and there is nothing that you can do about it.
+
+// // So if you really need to break out of a loop
+// // then you have to keep using the for of loop,
+
+// // but other than that
+// // it really comes down to your personal preference.
+// // Just like so many other things in JavaScript.
+// // So every programming language always has
+// // many different ways or different tools
+// // to achieve the same thing.
+// // And specially as more things
+// // keep getting added to the language
+// // there will always be more options to achieve the same result
+
+// // All right, and with this we wrap up this lecture.
+// // SO in my opinion this is a really brilliant
+// // and powerful mechanism,
+// // but I know that it's also rather complex
+// // to wrap your head around this, isn't it?
+// // And it actually took quite some time for me
+// // to fully understand this myself, back in the day.
+// // So please don't become frustrated with this,
+// // it will all become obvious with the practice.
+// // So just keep using this and then you will be fine.
+// // Once you do understand exactly how this works
+// // so specially this mechanism of the callback
+// // and of the passing arguments into this callback here
+// // so once you understand this fundamental mechanism
+// // then working with all other array methods
+// // in this section will become really easy.
+// // Because most of them follow the exact same principle
+// // of the callback function that we just explored here.
+// // So please review this lecture carefully, and yeah.
+// // If you really understand
+// // the role of the callback function here
+
+///////////////////////////////////////////////////////////////////
+// forEach With Maps and Sets
+///////////////////////////////////////////////////////////////////
+// So we learned about the forEach method on arrays.
+// However, forEach is also available on maps and sets.
+// And so let's take a small detour now
+// and see how forEach works with maps and with sets.
+
+// MAP
+
+// And remember that in this array of arrays,
+// each of these array elements, so this inner array,
+// will be one entry of the map,
+// where this here is the key -(USD), and this is the value (United States dollar).
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+// And so we can call forEach also on a map.
+// So forEach, and then again, the callback function,
+// and now this callback function also has three parameters.
+// So when the forEach method calls it,
+// it will call this function with three arguments.
+// So the first one will be the current value,
+// so the current value in the current iteration,
+// the second one is the key, and the third one
+// is the entire map that is being looped over.
+
+// And so you see, this is similar to the array,
+// where in the array,
+// the first parameter is the current element of the array,
+// the second one is the index
+// and the third is the entire array.
+// And so there is a nice correspondence between these,
+// and so this is quite easy to memorize here.
+
+currencies.forEach(function (value, key, map) {
+  console.log(`${key}: ${value}`);
 });
 
-// 0: function(200)
-// 1: function(450)
-// 2: function(400)
-// ...
+//SET
 
-// So basically this is exactly the concept that I explained
-// In the last section
-// when I said that we use a callback function
-// to tell another higher order function
-// exactly what it should do,
-// and so in this case we tell forEach
-// that in each iteration
-// it should log one of these two strings here to the console
-// So we give the forEach method instructions
-// by giving it a callback function
-// which contains these instructions, alright.
-// And I really know and understand
-// that this is quite a hard concept to wrap your head around.
-// But if you just continue using this from now on
-// then eventually it will start to make sense, believe me.
+const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
+console.log(currenciesUnique);
 
-// Well I think that we can both agree
-// that it is the forEach method, right.
-// And maybe you don't agree with that
-// and of course that's okay,
-// as I keep mentioning
-// it's always good to develop your own style of programming.
-
-// So entries is in fact just another array method
-// and it returns an array of arrays,
-// remember, which in the first position
-// contains the current index
-// and then the value itself.
-// And so this is how we access
-// the counter variable in the for of loop.
-for (const [i, movement] of movements.entries()) {
-  if (movement > 0) {
-    console.log(`Movement ${i + 1}: You deposited ${movement}`);
-  } else {
-    console.log(`Movement ${i + 1}: You withdraw ${Math.abs(movement)}`);
-  }
-}
-
-// And here, fortunately, it's a lot easier
-// to get access to the current index.
-// So to understand how it works we need to remember once more
-// that it is the forEach method
-// who calls this callback function in each iteration.
-// And as it calls this function
-// it also passes in the current element of the array,
-// but actually that's not all it passes in
-
-// in fact forEach passes in the current element,
-// the index and the entire array that we are looping.
-// And so therefore we can specify them here
-// in our parameter list.
-// So let's say index and array,
-
-// now of course we can just use one, like we have been doing,
-// or we can just use two,
-// or we can use all three together.
-// And as always the names here do not matter at all,
-// but what does matter is the order.
-// So the first parameter always needs to be
-// the current element,
-// the second parameter always the current index
-// and the third one always the entire array
-// that we are looping over.
-// Because that's the order in which the arguments,
-// so the actual values, are passed into our callback function.
-
-// And in fact in the real world we actually use
-// shorter names.
-console.log('------FOREACH with index-------');
-movements.forEach(function (mov, i, arr) {
-  if (mov > 0) {
-    console.log(`Movement ${i + 1}: You deposited ${mov}`);
-  } else {
-    console.log(`Movement ${i + 1}: You withdraw ${Math.abs(mov)}`);
-  }
+currenciesUnique.forEach(function (value, _, map) {
+  console.log(`${value}: ${value}`);
 });
 
-// Now when should you use forEach
-// and when should you use the for of loop.
+// And so what this means is that the key here
+// is exactly the same as the value.
+// So why is that?
 
-// Well one fundamental difference
-// between the two of them is that you cannot break out
-// of a forEach loop.
-// So the continue and break statements
-// do not work in a forEach loop at all.
-// So instead, forEach will always loop over the entire array
-// and there is nothing that you can do about it.
+// Well, a set doesn't have keys, right?
+// And it doesn't have indexes either.
+// And so there is no value that would make sense for the key.
+// All right, so essentially this key here
+// makes no sense at all.
+// It wouldn't even have to be there.
+// And so the people who designed this forEach method for sets,
+// they could have simply omitted the second argument, right?
 
-// So if you really need to break out of a loop
-// then you have to keep using the for of loop,
+// Well, if they did that,
+// then this forEach would have been different from the others.
+// And so that would then create confusion in developers,
+// and therefore it was decided to keep the same signature.
+// So basically to keep the same three parameters
+// in this callback function and simply
+// to set the second one to the first one.
+// So we can just write value here as well,
+// just to avoid that confusion.
 
-// but other than that
-// it really comes down to your personal preference.
-// Just like so many other things in JavaScript.
-// So every programming language always has
-// many different ways or different tools
-// to achieve the same thing.
-// And specially as more things
-// keep getting added to the language
-// there will always be more options to achieve the same result
-
-// All right, and with this we wrap up this lecture.
-// SO in my opinion this is a really brilliant
-// and powerful mechanism,
-// but I know that it's also rather complex
-// to wrap your head around this, isn't it?
-// And it actually took quite some time for me
-// to fully understand this myself, back in the day.
-// So please don't become frustrated with this,
-// it will all become obvious with the practice.
-// So just keep using this and then you will be fine.
-// Once you do understand exactly how this works
-// so specially this mechanism of the callback
-// and of the passing arguments into this callback here
-// so once you understand this fundamental mechanism
-// then working with all other array methods
-// in this section will become really easy.
-// Because most of them follow the exact same principle
-// of the callback function that we just explored here.
-// So please review this lecture carefully, and yeah.
-// If you really understand
-// the role of the callback function here
+// we cannot have the duplicate parameter name.
+// And so we can just use an underscore,
+// which in JavaScript means a throwaway variable.
+// So that means a variable that is completely unnecessary.
+// So it's just a convention which we will see again
+// a little bit later.
