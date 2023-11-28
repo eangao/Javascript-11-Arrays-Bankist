c287e696f4318f560051a74ac1a97fcbb6c396da
elmar <elmarcera@gmail.com>
2023-11-28 12:10:10 +0800

    Data Transformations: map, filter, reduce

--- script.js
+++ script.js
@@ -654,73 +654,95 @@ displayMovements(account1.movements);
 // Coding Challenge #1
 ///////////////////////////////////////////////////////////////
 
-// Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners
-// about their dog's age, and stored the data into an array (one array for each). For
-// now, they are just interested in knowing whether a dog is an adult or a puppy.
-// A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years
-// old.
+// // Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners
+// // about their dog's age, and stored the data into an array (one array for each). For
+// // now, they are just interested in knowing whether a dog is an adult or a puppy.
+// // A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years
+// // old.
 
-// Your tasks:
+// // Your tasks:
 
-// Create a function 'checkDogs', which accepts 2 arrays of dog's ages
-// ('dogsJulia' and 'dogsKate'), and does the following things:
+// // Create a function 'checkDogs', which accepts 2 arrays of dog's ages
+// // ('dogsJulia' and 'dogsKate'), and does the following things:
 
-// 1. Julia found out that the owners of the first and the last two dogs actually have
-// cats, not dogs! So create a shallow copy of Julia's array, and remove the cat
-// ages from that copied array (because it's a bad practice to mutate function
-// parameters)
+// // 1. Julia found out that the owners of the first and the last two dogs actually have
+// // cats, not dogs! So create a shallow copy of Julia's array, and remove the cat
+// // ages from that copied array (because it's a bad practice to mutate function
+// // parameters)
 
-// 2. Create an array with both Julia's (corrected) and Kate's data
+// // 2. Create an array with both Julia's (corrected) and Kate's data
 
-// 3. For each remaining dog, log to the console whether it's an adult ("Dog number 1
-// is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a puppy
-// �
-// ")
+// // 3. For each remaining dog, log to the console whether it's an adult ("Dog number 1
+// // is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a puppy
+// // �
+// // ")
 
-// 4. Run the function for both test datasets
+// // 4. Run the function for both test datasets
 
-// Test data:
-// § Data 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
-// § Data 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
+// // Test data:
+// // § Data 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
+// // § Data 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
 
-// Hints: Use tools from all lectures in this section so far �
-// GOOD LUCK �
+// // Hints: Use tools from all lectures in this section so far �
+// // GOOD LUCK �
 
-const checkDogs = function (dogsJulia, dogsKate) {
-  // 1.
+// const checkDogs = function (dogsJulia, dogsKate) {
+//   // 1.
 
-  // My solutions
-  // const dogsJuliaCorrected = [...dogsJulia];
-  // console.log(dogsJuliaCorrected);
-  // dogsJuliaCorrected.shift();
-  // console.log(dogsJuliaCorrected);
-  // dogsJuliaCorrected.pop();
-  // console.log(dogsJuliaCorrected);
-  // dogsJuliaCorrected.pop();
-  // console.log(dogsJuliaCorrected);
+//   // My solutions
+//   // const dogsJuliaCorrected = [...dogsJulia];
+//   // console.log(dogsJuliaCorrected);
+//   // dogsJuliaCorrected.shift();
+//   // console.log(dogsJuliaCorrected);
+//   // dogsJuliaCorrected.pop();
+//   // console.log(dogsJuliaCorrected);
+//   // dogsJuliaCorrected.pop();
+//   // console.log(dogsJuliaCorrected);
 
-  // Instructor solution
-  const dogsJuliaCorrected = dogsJulia.slice();
-  dogsJuliaCorrected.splice(0, 1);
-  dogsJuliaCorrected.splice(-2);
-  console.log(dogsJuliaCorrected);
+//   // Instructor solution
+//   const dogsJuliaCorrected = dogsJulia.slice();
+//   dogsJuliaCorrected.splice(0, 1);
+//   dogsJuliaCorrected.splice(-2);
+//   console.log(dogsJuliaCorrected);
 
-  //2
-  //My solution
-  // const dogs = [...dogsJuliaCorrected, ...dogsKate];
+//   //2
+//   //My solution
+//   // const dogs = [...dogsJuliaCorrected, ...dogsKate];
 
-  //intructor solution
-  const dogs = dogsJuliaCorrected.concat(dogsKate);
-  console.log(dogs);
+//   //intructor solution
+//   const dogs = dogsJuliaCorrected.concat(dogsKate);
+//   console.log(dogs);
 
-  dogs.forEach(function (dog, i) {
-    if (dog >= 3) {
-      console.log(`Dog number ${i + 1} is an adult, and is ${dog} years old`);
-    } else {
-      console.log(`Dog number ${i + 1} is still a puppy 🐶`);
-    }
-  });
-};
+//   dogs.forEach(function (dog, i) {
+//     if (dog >= 3) {
+//       console.log(`Dog number ${i + 1} is an adult, and is ${dog} years old`);
+//     } else {
+//       console.log(`Dog number ${i + 1} is still a puppy 🐶`);
+//     }
+//   });
+// };
 
-checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
-checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
+// checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+// checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
+
+//////////////////////////////////////////////////////////
+// Data Transformations: map, filter, reduce
+//////////////////////////////////////////////////////////
+
+// In JavaScript,
+// there are three big and important array methods
+// that we use all the time to perform data transformations.
+// So basically,
+// these are methods that we use
+// to create new arrays based
+// on transforming data from other arrays.
+// And in recent years,
+// these tools have become really popular and for good reasons
+// and therefore you'll see them everywhere you look
+// in Modern JavaScript.
+// And the tools I'm talking about are;
+// map,
+// filter
+// and reduce.
+
+// SEE PDF LECTURE
